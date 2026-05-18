156ae3b33fae91280aac02a95e3cb24f09ee1c2f
Nidal-Aamir <aamir.nidal91@gmail.com>
2026-05-18 16:40:31 +0200

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+-- Create Database 'DataWarehouse'
+USE master;
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE Schema bronze;
+GO
+
+CREATE Schema silver;
+GO
+
+CREATE Schema gold;
+GO
