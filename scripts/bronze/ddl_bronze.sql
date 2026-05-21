3c917d9f1477d4eeaf4b02fc192c95f0c3b0b944
Nidal-Aamir <aamir.nidal91@gmail.com>
2026-05-21 17:57:57 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -49,57 +49,6 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_price INT
 );
 
-/*
-================================================================
-DDL Script: Create Bronze Tables
-================================================================
-Script Purpose:
-    This script creates tables within the 'bronze' schema.
-    If the tables already exist, they are dropped and recreated.
-    
-    Run this script to redefine the DDL structure of the 'bronze' layer tables.
-
-*/
-
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_cust_info;
-CREATE TABLE bronze.crm_cust_info (
-	cst_id INT,
-	cst_key NVARCHAR(50),
-	cst_firstname NVARCHAR(50),
-	cst_lastname NVARCHAR(50),
-	cst_marital_status NVARCHAR(50),
-	cst_gndr NVARCHAR(50),
-	cst_create_date DATE
-);
-
-IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_prd_info;
-CREATE TABLE bronze.crm_prd_info (
-	prd_id INT,
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
-	prd_cost INT, 
-	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME
-);
-
-
-IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_sales_details;
-CREATE TABLE bronze.crm_sales_details (
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT
-);
-
 
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
